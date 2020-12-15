d9161355f9ae46444edefbb7a9ac06914b0311eb
Elias Gemperle <elias.gemperle@roche.com>
2020-12-15 13:06:53 +0100

    Updates solution

--- 15_day/puzzle_1_and_2.py
+++ 15_day/puzzle_1_and_2.py
@@ -2,13 +2,10 @@
 import sys
 
 numbers = [int(number) for number in open('input').read().split(",")]
-last_occurence = {number:i + 1 for i, number in enumerate(numbers[:-1])}
+last_occurence = {number:i + 1 for i, number in enumerate(int(number) for number in open('input').read().split(",")[:-1])}
 
-for i in range(len(numbers), int(sys.argv[1])):
-    if numbers[-1] not in last_occurence:
-        numbers.append(0)
-    else:
-        numbers.append(i - last_occurence[numbers[-1]])
+for i in range(len(last_occurence) + 1, int(sys.argv[1])):
+    numbers.append(0 if numbers[-1] not in last_occurence else i - last_occurence[numbers[-1]])
     last_occurence[numbers[-2]] = i
 
 print(numbers[-1])
