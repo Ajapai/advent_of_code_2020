dfe53f9ee683b3c52e8d083f551341466ff25282
Elias Gemperle <elias.gemperle@roche.com>
2020-12-15 13:19:11 +0100

    Replaces list with last_number

--- 15_day/puzzle_1_and_2.py
+++ 15_day/puzzle_1_and_2.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 import sys
 
-numbers = [int(number) for number in open('input').read().split(",")]
-last_occurence = {number:i + 1 for i, number in enumerate(int(number) for number in open('input').read().split(",")[:-1])}
+last_number = [int(number) for number in open('input').read().split(",")][-1]
+last_occurences = {number:i + 1 for i, number in enumerate(int(number) for number in open('input').read().split(",")[:-1])}
 
-for i in range(len(last_occurence) + 1, int(sys.argv[1])):
-    numbers.append(0 if numbers[-1] not in last_occurence else i - last_occurence[numbers[-1]])
-    last_occurence[numbers[-2]] = i
+for i in range(len(last_occurences) + 1, int(sys.argv[1])):
+    current_number = 0 if last_number not in last_occurences else i - last_occurences[last_number]
+    last_occurences[last_number] = i
+    last_number = current_number
 
-print(numbers[-1])
+print(last_number)
